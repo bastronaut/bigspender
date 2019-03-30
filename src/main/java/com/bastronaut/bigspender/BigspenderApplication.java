31ce0df1c4e505a9246beeef4623de01ebedd394
bastronaut <btsijtsma@gmail.com>
2019-03-30 23:48:41 +0100

    Initial commit

---
+++ src/main/java/com/bastronaut/bigspender/BigspenderApplication.java
@@ -0,0 +1,13 @@
+package com.bastronaut.bigspender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigspenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigspenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bastronaut/bigspender/BigspenderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bastronaut.bigspender;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BigspenderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
